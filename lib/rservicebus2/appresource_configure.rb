b5494d4253b02178920b674be0a3c312ab6d85d7
guyirvine <guyirvine@gmail.com>
2021-05-11 05:07:46 +1200

    rubocop appresource_configure

--- lib/rservicebus2/appresource_configure.rb
+++ lib/rservicebus2/appresource_configure.rb
@@ -17,8 +17,8 @@ module RServiceBus2
           k = k.sub('RSBFDB2_', '')
           rm.add k, AppResourceFluidDb2.new(host, uri)
         elsif v.is_a?(String) &&
-              (k.start_with?('RSBFDB_') || v.index('fluiddb') == 0)
-          v = v['fluiddb'.length..-1] if v.index('fluiddb') == 0
+              (k.start_with?('RSBFDB_') || v.start_with?('fluiddb'))
+          v = v['fluiddb'.length..-1] if v.start_with?('fluiddb')
           uri = URI.parse(v)
           require 'rservicebus2/appresource/fluiddb'
 
