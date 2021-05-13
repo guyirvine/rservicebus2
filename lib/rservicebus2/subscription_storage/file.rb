4f313fc3b661ea61b58b7d78d8f1ffcd83e5e29b
guyirvine <guyirvine@gmail.com>
2021-05-13 17:25:38 +1200

    rubocop subscription_storage_file

--- lib/rservicebus2/subscription_storage/file.rb
+++ lib/rservicebus2/subscription_storage/file.rb
@@ -1,15 +1,9 @@
+# frozen_string_literal: true
+
 module RServiceBus2
   # Implementation of Subscription Storage to Redis
   class SubscriptionStorageFile < SubscriptionStorage
-    # Constructor
-    #
-    # @param [String] app_ame Name of the application, used as a Namespace
-    # @param [String] uri resource location to attach, eg redis://127.0.0.1/foo
-    def initialize(app_name, uri)
-      super(app_name, uri)
-    end
-
-    def get_all
+    def all
       RServiceBus2.log 'Load subscriptions'
       return {} unless File.exist?(@uri.path)
 
@@ -17,13 +11,7 @@ module RServiceBus2
     end
 
     def add(event_name, queue_name)
-      # s => subscriptions
-      if File.exist?(@uri.path)
-        s = YAML.load(File.open(@uri.path))
-      else
-        s = {}
-      end
-
+      s = File.exist?(@uri.path) ? YAML.load(File.open(@uri.path)) : {}
       s[event_name] = [] if s[event_name].nil?
 
       s[event_name] << queue_name
@@ -35,8 +23,7 @@ module RServiceBus2
     end
 
     def remove(_event_name, _queue_name)
-      fail 'Method, remove, needs to be implemented for this
-            subscription storage'
+      raise 'Method, remove, needs to be implemented for this subscription storage'
     end
   end
 end
