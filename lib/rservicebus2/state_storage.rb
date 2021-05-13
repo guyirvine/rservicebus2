6535f1214579c7be157aa79ddaa6ec0fe7fe904e
guyirvine <guyirvine@gmail.com>
2021-05-13 17:13:03 +1200

    rubocop state_storage

--- lib/rservicebus2/state_storage.rb
+++ lib/rservicebus2/state_storage.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 module RServiceBus2
   # State Storage
   class StateStorage
     def self.get(uri)
       case uri.scheme
       when 'dir'
-        require 'rservicebus2/state_storage/dir.rb'
-        return StateStorageDir.new(uri)
+        require 'rservicebus2/state_storage/dir'
+        StateStorageDir.new(uri)
       when 'inmem'
-        require 'rservicebus2/state_storage/inmemory.rb'
-        return StateStorageInMemory.new(uri)
+        require 'rservicebus2/state_storage/inmemory'
+        StateStorageInMemory.new(uri)
       else
-        abort("Scheme, #{uri.scheme}, not recognised when configuring
-          StateStorage, #{uri}")
+        abort("Scheme, #{uri.scheme}, not recognised when configuring StateStorage, #{uri}")
       end
     end
   end
