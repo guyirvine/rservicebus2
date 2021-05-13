5aab2f5e34372554adee9df65f05f6d47b1485f3
guyirvine <guyirvine@gmail.com>
2021-05-13 18:13:22 +1200

    lib/rservicebus2/message/*

--- lib/rservicebus2/message/subscription.rb
+++ lib/rservicebus2/message/subscription.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module RServiceBus2
   # Class to hold message subscriptions
   class MessageSubscription
--- lib/rservicebus2/message/verboseoutput.rb
+++ lib/rservicebus2/message/verboseoutput.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module RServiceBus2
   class MessageVerboseOutputOn
   end
