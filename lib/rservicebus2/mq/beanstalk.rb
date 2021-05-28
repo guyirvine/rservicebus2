5b715d7625362e24bf36422219a253f629219d91
guyirvine <guyirvine@gmail.com>
2021-05-28 14:29:56 +1200

    Update mq/beanstalk messaging

--- lib/rservicebus2/mq/beanstalk.rb
+++ lib/rservicebus2/mq/beanstalk.rb
@@ -16,19 +16,17 @@ module RServiceBus2
         @beanstalk = Beanstalk::Pool.new([string])
         @max_job_size = @beanstalk.stats['max-job-size']
         if @max_job_size < 4_194_304
-          puts "***WARNING: Lowest recommended.max-job-size is 4m, current
-                max-job-size, #{@max_job_size.to_f / (1024 * 1024)}m"
-          puts '***WARNING: Set the job size with the -z switch, eg
-                /usr/local/bin/beanstalkd -z 4194304'
+          puts '***WARNING: Lowest recommended.max-job-size is 4m, current ' \
+                "max-job-size, #{@max_job_size.to_f / (1024 * 1024)}m"
+          puts '***WARNING: Set the job size with the -z switch, eg ' \
+                '/usr/local/bin/beanstalkd -z 4194304'
         end
       rescue StandardError => e
         puts 'Error connecting to Beanstalk'
         puts "Host string, #{string}"
         if e.message == 'Beanstalk::NotConnected'
-          puts '***Most likely, beanstalk is not running. Start beanstalk,
-                  and try running this again.'
-          puts "***If you still get this error, check beanstalk is running
-                  at, #{string}"
+          puts '***Most likely, beanstalk is not running. Start beanstalk, and try running this again.'
+          puts "***If you still get this error, check beanstalk is running at, #{string}"
         else
           puts e.message
           puts e.backtrace
